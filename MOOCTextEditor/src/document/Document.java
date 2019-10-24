6f26272acc0ba3c2c20acfb5a37b0bff5e48ed0d
Jane <Jane@10.0.68.188>
2019-10-23 19:41:31 -0500

    Changes to flesch score

--- MOOCTextEditor/src/document/Document.java
+++ MOOCTextEditor/src/document/Document.java
@@ -130,7 +130,7 @@ public abstract class Document {
 	/** return the Flesch readability score of this document */
 	public double getFleschScore()
 	{
-	    double fleschScore = (206.835-(1.015*(getNumWords()/getNumSentences())-84.6*(getNumSyllables()/getNumWords())));
+	    double fleschScore = (206.835-(1.015*((double) getNumWords()/getNumSentences())-84.6*((double) getNumSyllables()/getNumWords())));
 	    return fleschScore;
 	}
 	
